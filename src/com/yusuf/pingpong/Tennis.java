f6e6c25cdc890b88bca9a26220de24b3b2c9e6fe
Yusuf Ismail <yusufgamer222@gmail.com>
2021-07-25 11:50:37 +0100

    Update Tennis.java

--- src/com/yusuf/pingpong/Tennis.java
+++ src/com/yusuf/pingpong/Tennis.java
@@ -2,18 +2,64 @@ package com.yusuf.pingpong;
 
 import java.applet.Applet;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Tennis extends Applet {
+public class Tennis extends Applet implements Runnable, KeyListener {
     final int WIDTH = 700, HEIGHT = 500;
+    Thread thread;
+
     public void init() {
         this.resize(WIDTH, HEIGHT);
+
+        this.addKeyListener(this);
+
+        thread = new Thread(this);
+        thread.start();
     }
 
     public void paint(Graphics g) {
-
+        g.setColor(Color.black);
+        g.fillRect(0, 0, WIDTH, HEIGHT);
     }
 
     public void update(Graphics g) {
         paint(g);
+    }
+
+
+    public void run() {
+        for(;;) {
+
+
+            repaint();
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    public void keyPressed(KeyEvent e) {
+        if(e.getKeyCode() == KeyEvent.VK_W) {
+
+        }
+        else if (e.getKeyCode() == KeyEvent.VK_S) {
+
+        }
+    }
+
+    public void keyReleased(KeyEvent e) {
+        if(e.getKeyCode() == KeyEvent.VK_W) {
+
+        }
+        else if (e.getKeyCode() == KeyEvent.VK_S) {
+
+        }
     }
 }
